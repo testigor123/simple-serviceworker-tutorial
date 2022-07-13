12bd4a2508fcfd06c4926bda29aae8bdbdffaab6
Igor Bragaia <igorbragaia@microsoft.com>
2022-07-13 15:45:24 -0700

    Go

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var p = document.createElement('p');
 p.textContent = 'This content was added via JavaScript!';
 document.body.appendChild(p);
+
+chrome.edgePanel.basicPageContext.onUpdated.addListener((basicContext) => {
+    console.log(basicContext);
+    document.getElementsByTagName("h1")[0].innerText = basicContext.title;
+});
